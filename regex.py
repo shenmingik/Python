dbf87b83b7f1bbd4bb1f88a9180862955a7f8d0a
ik-ubuntu <2107810343@qq.com>
2021-05-17 22:22:10 +0800

    learn regax-3

--- regex.py
+++ regex.py
@@ -5,9 +5,16 @@
 # mo = phone_number.search("zxc123-456-7895")
 # print(mo.group()) #print 123-456-7895
 
+# import re
+# phone_number_regax = re.compile("(\d\d\d)-(\d\d\d-\d\d\d\d)")
+# mo = phone_number_regax.search("zxc123-456-7895")
+# print(mo.group(0))  # print 123-456-7895
+# print(mo.group(1))  # print 123
+# print(mo.group(2))  # print 456-7895
+
 import re
-phone_number_regax = re.compile("(\d\d\d)-(\d\d\d-\d\d\d\d)")
-mo = phone_number_regax.search("zxc123-456-7895")
-print(mo.group(0))
-print(mo.group(1))  # print 123
-print(mo.group(2))  # print 456-7895
+grep_regax = re.compile("A|B")  # 匹配A 或 B
+mo = grep_regax.search("A123456")
+print(mo.group())   # 打印出A
+mo1 = grep_regax.search("B123456")
+print(mo1.group()) # 打印出B
