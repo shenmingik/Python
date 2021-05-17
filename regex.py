7bceec012d16abb6a7866de952d6708f70d80ecf
ik-ubuntu <2107810343@qq.com>
2021-05-17 22:13:19 +0800

    learn regax-2

--- regex.py
+++ regex.py
@@ -1,6 +1,13 @@
+# import re
+
+# phone_number = re.compile("\d\d\d-\d\d\d-\d\d\d\d")
+
+# mo = phone_number.search("zxc123-456-7895")
+# print(mo.group()) #print 123-456-7895
+
 import re
-
-phone_number = re.compile("\d\d\d-\d\d\d-\d\d\d\d")
-
-mo = phone_number.search("zxc123-456-7895")
-print(mo.group())
+phone_number_regax = re.compile("(\d\d\d)-(\d\d\d-\d\d\d\d)")
+mo = phone_number_regax.search("zxc123-456-7895")
+print(mo.group(0))
+print(mo.group(1))  # print 123
+print(mo.group(2))  # print 456-7895
