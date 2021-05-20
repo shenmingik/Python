e9e11b7dd572df211d02ca836eb6f4a2f07cec9b
ik-ubuntu <2107810343@qq.com>
2021-05-20 15:48:59 +0800

    删除文件和文件夹

--- orignize_files.py
+++ orignize_files.py
@@ -11,8 +11,31 @@
 
 # 移动文件和文件夹
 ##################################################################
+# import shutil
+# import os
+
+# current_path = os.getcwd()
+# # 将temp2文件夹内的文件拷贝到temp中,并将temp2改名为temp
+# shutil.move(current_path+"/Python/temp2",current_path+"/temp")
+
+# 删除文件和文件夹
+##################################################################
+# import shutil
+# import os
+
+# current_path = os.getcwd()
+# shutil.rmtree(current_path+"/temp")
+
+# 删除文件和文件夹
+##################################################################
 import shutil
 import os
 
 current_path = os.getcwd()
-shutil.move(current_path+"/Python/temp2",current_path+"/temp")
+for folder_name,sub_folders,file_names in os.walk(current_path):
+    print(folder_name+":")
+    # 加载当前文件路径下的所有子文件
+    for sub_folder in sub_folders:
+        print("\t"+folder_name+": "+sub_folder+"(dir)")
+    for file_name in file_names:
+        print("\t"+folder_name+": "+file_name)
