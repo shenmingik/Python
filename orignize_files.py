f6be9ed65643fe2fc5fca52bd61646b8fd9991bc
ik-ubuntu <2107810343@qq.com>
2021-05-20 15:17:49 +0800

    移动文件和文件夹

--- orignize_files.py
+++ orignize_files.py
@@ -1,10 +1,18 @@
 # 拷贝文件和文件夹
 ##################################################################
+# import shutil
+# import os
+
+# current_path = os.getcwd()
+# # 拷贝test.txt 文件到temp1文件夹下
+# shutil.copy(current_path+"/test.txt",current_path+"/Python/temp1")
+# # 将temp1文件夹内容拷贝到temp2文件夹下,此时会创建temp2文件
+# shutil.copytree(current_path+"/Python/temp1",current_path+"/Python/temp2")
+
+# 移动文件和文件夹
+##################################################################
 import shutil
 import os
 
 current_path = os.getcwd()
-# 拷贝test.txt 文件到temp1文件夹下
-shutil.copy(current_path+"/test.txt",current_path+"/Python/temp1")
-# 将temp1文件夹内容拷贝到temp2文件夹下,此时会创建temp2文件
-shutil.copytree(current_path+"/Python/temp1",current_path+"/Python/temp2")
+shutil.move(current_path+"/Python/temp2",current_path+"/temp")
