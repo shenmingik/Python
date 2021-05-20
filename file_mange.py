00e13a0dedda33f00c136a852ab97005978900d7
ik-ubuntu <2107810343@qq.com>
2021-05-20 12:59:48 +0800

    创建文件夹

--- file_mange.py
+++ file_mange.py
@@ -1,8 +1,16 @@
 # 获取与改变当前路径
 ##################################################################
+# import os
+# # 获取当前路径
+# print(os.getcwd())
+# # 改变当前路径
+# os.chdir("/home/ubuntu")
+# print(os.getcwd())
+
+# 获取与改变当前路径
+##################################################################
 import os
 # 获取当前路径
-print(os.getcwd())
-# 改变当前路径
-os.chdir("/home/ubuntu")
-print(os.getcwd())
+current_path = os.getcwd()
+# 在当前路径下创建file_manage 和中间文件夹files
+os.makedirs(current_path+"/files/file_manage")
