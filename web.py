759ae4b0837f213bfdda3ff36e0e415c533445bb
ik-ubuntu <2107810343@qq.com>
2021-05-21 15:04:24 +0800

    获取命令行参数

--- web.py
+++ web.py
@@ -1,4 +1,14 @@
 # 启动新浏览器
 ##################################################################
+# import webbrowser
+# webbrowser.open("https://so.csdn.net/so/search?q=进程&t=blog&u=shenmingxueIT")
+
+# 获取命令行参数
+##################################################################
+import sys
 import webbrowser
-webbrowser.open("https://so.csdn.net/so/search?q=进程&t=blog&u=shenmingxueIT")
+# 如果有输出参数
+if len(sys.argv) > 1:
+    content = "".join(sys.argv[1])
+webbrowser.open("https://so.csdn.net/so/search?q="+content+"&t=blog&u=shenmingxueIT")
+
