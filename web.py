9c438fa0430cf430fb3a6e312e1856730e042a7c
ik-ubuntu <2107810343@qq.com>
2021-05-21 17:24:21 +0800

    下载页面到文件

--- web.py
+++ web.py
@@ -24,10 +24,22 @@
 
 # 检查错误
 ##################################################################
-import requests
-# 一个错误的url
-res = requests.get("http://inventwithpython.com/123456")
-try:
-    res.raise_for_status()
-except Exception as exc:
-    print("There was a problem: %s" %(exc))
+# import requests
+# # 一个错误的url
+# res = requests.get("http://inventwithpython.com/123456")
+# try:
+#     res.raise_for_status()
+# except Exception as exc:
+#     print("There was a problem: %s" %(exc))
+
+# 下载页面到文件
+##################################################################
+# import requests
+# res = requests.get("http://www.gutenberg.org/cache/epub/1112/pg1112.txt")
+# res.raise_for_status()
+# down_file = open("Romo&Juliet.txt","wb")
+# # iter_content()方法在循环的每次迭代中，返回一段内容。每一段都是bytes数据类型，我们需要指定一段包含多少字节
+# for chunk in res.iter_content(50000):
+#     down_file.write(chunk)
+
+# down_file.close()
